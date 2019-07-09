b7e5dd1a8d2bd69d456de6cbeca71cea4f6e64f4
turgutumec <turgutumec@TURGUTUMEAB0D>
2019-07-09 11:14:58 +0300

    Start

---
+++ NetCamp/NetCamp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NetCamp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
